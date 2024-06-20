13957147c8248e202f4e696e32cda4b61259850c
florenciaquilodran <florenciaquilodran@gmail.com>
2024-06-20 01:27:05 -0300

    se achica imagen de main, se acomodan imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import Main from './components/Main'
 import Footer from './components/Footer'
 import './index.css';
-//import { BrowserRouter } from "react-router-dom";
+
 
 function App() {
   
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,7 +7,7 @@ function Main(){
         
         <main className="p-4 bg-[rgba(255,255,255,0.2)] grow">
             <h2>Home</h2>
-            <img src="http://www.bolundberg.com/wp-content/uploads/2024/01/Epstein_eget-3-1.jpg" alt="imagen" />
+            <img src="http://www.bolundberg.com/wp-content/uploads/2024/01/Epstein_eget-3-1.jpg" alt="imagen" width={100}/>
             <p>Here you can find something awesome for your space</p>
             <Button texto="ver mas"/>
             <Button texto="detalles"/>
