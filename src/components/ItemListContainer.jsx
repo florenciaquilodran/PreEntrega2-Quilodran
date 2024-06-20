7db332317d15dd0bad8f9dee4be480227a91cc65
florenciaquilodran <florenciaquilodran@gmail.com>
2024-06-20 00:16:32 -0300

    Se agrega useEffect para que no cargue constantemente los productos

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,14 +1,21 @@
 import ItemList from "./ItemList";
 import { useState } from "react";
+import { useEffect } from "react";
 
 function ItemListContainer(){
     
     
-    const [estado, setEstado] = useState()
+    const [estado, setEstado] = useState([])
 
-    fetch('https://dummyjson.com/products')
-    .then((res) => {return res.json()})
-    .then((data) => {console.log(data.products)});
+
+    useEffect(()=>{
+        fetch('https://dummyjson.com/products')
+        .then((res) => {return res.json()})
+        .then((data) => {console.log(data.products)
+            setEstado(data.products)
+        })
+
+    }, [])
     
     return (
             <ItemList/>        
